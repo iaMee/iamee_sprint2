f54ac9a16e63914419e29485dec5ddaeb86030a5
Divij <divij.mehra.16@gmail.com>
2017-06-25 13:18:25 +1000

    Add Templates Organisms and Pages for folder structure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import BaseButton from './components/Atoms/Buttons/BaseButton'
+import BaseButton from './components/Atoms/Buttons/BaseButton';
+import BottomNav from './components/Molecules/BottomNav/BottomNav';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-          <BaseButton
-          title='Sign Up'/>
+      <Router>
+        <div>
+            <Route exact path="/" component={Homepage}/>
+           <Route path="/signup" component={SignUp}/>
+           <Route path="/experiences" component={ExperiencePage}/>
+           <Route path="/createtask" component={CreateTask}/>
+           <Route path="/dashboard" component={Dashboard}/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/Molecules/BottomNav/BottomNav.js
+++ src/components/Molecules/BottomNav/BottomNav.js
@@ -1,43 +1,63 @@
 import React, { Component } from 'react';
+
 import style from './BottomNav.css';
-import { Footer } from 'react-materialize'
-import NavIcon from '../../Atoms/NavIcons/NavIcon'
+import { Footer } from 'react-materialize';
 
+import NavIcon from '../../Atoms/NavIcons/NavIcon';
+import PropTypes from 'prop-types';
+
+import FaHome from 'react-icons/lib/fa/home';
+import FaBell from 'react-icons/lib/fa/bell';
+import FaPlusCircle from 'react-icons/lib/fa/plus-circle';
+import FaSearch from 'react-icons/lib/fa/search';
+import FaUsers from 'react-icons/lib/fa/user';
+
+// BottomNav which has NavIcon components. It requires a redirect path, iconImage and text
 class BottomNav extends Component {
-
-  pathRedirect() {
-    
-  }
-
   render() {
+
+    var FaIcon = React.createClass({
+      render: function() {
+        return <FaHome />
+      }
+    })
+
     return (
-      <Footer>
+      <Footer className="navfooter">
         <NavIcon
-        onClick={pathRedirect}
-        iconImage=""
-        text=
+          path="/home"
+          iconImage={FaIcon}
+          text="Home"
         />
         <NavIcon
-        onClick={pathRedirect}
-        iconImage=""
-        text=
+          path="/notifications"
+          iconImage={FaIcon}
+          text="Notifications"
         />
         <NavIcon
-        onClick={pathRedirect}
-        iconImage=""
-        text=
+          path="/create"
+          iconImage={FaIcon}
+          text="Create"
         />
         <NavIcon
-        onClick={pathRedirect}
-        iconImage=""
-        text=
+          path="/find"
+          iconImage={FaIcon}
+          text="Find"
         />
         <NavIcon
-        onClick={pathRedirect}
-        iconImage=""
-        text=
+          path="/buddies"
+          iconImage={FaIcon}
+          text="Buddies"
         />
       </Footer>
     );
   }
 }
+
+// BottomNav.propTypes = {
+//   path: PropTypes.string.isRequired,
+//   iconImage: PropTypes.element.isRequired,
+//   text: PropTypes.string.isRequired
+// }
+
+export default BottomNav;
