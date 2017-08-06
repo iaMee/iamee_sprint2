698a976af5ddc3cbb148b1f1a66008fff3d0a839
Andi <andi.winata@hotmail.com>
2017-08-06 20:56:58 +1000

    Fixing lint error

--- src/Atoms/Buttons/BaseButton.js
+++ src/Atoms/Buttons/BaseButton.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import style from './BaseButton.css';
 import { Button } from 'react-materialize';
 
-const BaseButton = ({ className, onClick, title }) =>
+const BaseButton = ({
+  className,
+  onClick,
+  title
+  }) =>
   <Button onClick={onClick} className={`base-button ${className}`}>
     {title}
   </Button>;
