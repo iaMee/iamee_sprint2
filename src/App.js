2b31af63ac80aa39bc20194f451d51b38adc374c
Andi <andi.winata@hotmail.com>
2017-08-09 08:21:13 +1000

    Adding prettier

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
-import "./App.css";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import createBrowserHistory from "history/createBrowserHistory";
-import Home from "./Templates/Home/Home";
-import SignUp from "./Templates/SignUp/SignUp";
-import ExperiencePage from "./Pages/ExperiencePage";
-import Test from "./Templates/Test/Test";
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
+import Home from './Templates/Home/Home';
+import SignUp from './Templates/SignUp/SignUp';
+import ExperiencePage from './Pages/ExperiencePage';
+import Test from './Templates/Test/Test';
 
 const history = createBrowserHistory();
 
